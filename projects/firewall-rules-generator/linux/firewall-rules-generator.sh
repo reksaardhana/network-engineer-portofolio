f4edd4f9a22fab53bc2874cacb995e2377a1a227
ReksaArdhana <158231790+ReksaArdhana@users.noreply.github.com>
2024-09-18 00:04:53 +0700

    Update firewall-rules-generator.sh

--- projects/firewall-rules-generator/linux/firewall-rules-generator.sh
+++ projects/firewall-rules-generator/linux/firewall-rules-generator.sh
@@ -1,1 +1,12 @@
+```bash
+#!/bin/bash
 
+# Menambahkan aturan untuk mengizinkan akses ke port 80 dan 443
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Menolak semua akses yang tidak diizinkan
+iptables -A INPUT -j DROP
+
+# Menyimpan aturan
+iptables-save > /etc/iptables/rules.v4
